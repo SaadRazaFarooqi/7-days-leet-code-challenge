77f0c9e9a7525910c86430dfc93ea32633be509c
SaadRazaFarooqi <165579262+SaadRazaFarooqi@users.noreply.github.com>
2024-05-04 19:07:43 +0500

    Time: 88 ms (59.79%), Space: 57.4 MB (28.87%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.js
@@ -0,0 +1,16 @@
+function reconstructQueue(people) {
+    
+    people.sort((a, b) => {
+        if (a[0] === b[0]) {
+            return a[1] - b[1] 
+        }
+        return b[0] - a[0]
+    })
+
+    const queue = [];
+    for (const person of people) {
+        queue.splice(person[1], 0, person)
+    }
+
+    return queue
+}
